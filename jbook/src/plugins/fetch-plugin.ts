e262783fa41d45f995ddc7c077f215185219d0ba
Gary Hake <Gary.Hake@waystar.com>
2021-04-30 10:44:30 -0400

    refactoring builds and esbuild.wasm

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -10,14 +10,14 @@ export const fetchPlugin = (inputCode: string) => {
 	return {
 		name: 'fetch-plugin',
 		setup(build: esbuild.PluginBuild) {
-			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				if (args.path === 'index.js') {
-					return {
-						loader: 'jsx',
-						contents: inputCode,
-					};
-				}
+			build.onLoad({ filter: /(^index\.js$)/ }, () => {
+				return {
+					loader: 'jsx',
+					contents: inputCode,
+				};
+			});
 
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
 					args.path
 				);
@@ -25,26 +25,36 @@ export const fetchPlugin = (inputCode: string) => {
 				if (cachedResult) {
 					return cachedResult;
 				}
+			});
 
+			build.onLoad({ filter: /.css$/ }, async (args: any) => {
 				const { data, request } = await axios.get(args.path);
-				const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-
 				const escaped = data
 					.replace(/\n/g, '')
 					.replace(/"/g, '\\"')
 					.replace(/'/g, "\\'");
-				const contents =
-					fileType === 'css'
-						? `
+				const contents = `
                     const style = document.createElement('style');
                     style.innerText = '${escaped}';
                     document.head.appendChild(style);
-                    `
-						: data;
+                    `;
 
 				const result: esbuild.OnLoadResult = {
 					loader: 'jsx',
 					contents: contents,
+					resolveDir: new URL('./', request.responseURL).pathname,
+				};
+
+				await fileCache.setItem(args.path, result);
+
+				return result;
+			});
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
+				const { data, request } = await axios.get(args.path);
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents: data,
 					resolveDir: new URL('./', request.responseURL).pathname,
 				};
 
