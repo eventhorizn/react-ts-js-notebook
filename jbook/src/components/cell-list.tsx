5a13e09a0285392bceae19d553ceb74c797f6ea7
Gary Hake <Gary.Hake@waystar.com>
2021-05-07 22:37:40 -0400

    creating typed selector for cell list rendering list of cells

--- jbook/src/components/cell-list.tsx
+++ jbook/src/components/cell-list.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-lilst-item';
 
 const CellList: React.FC = () => {
-	return <div>Cell List</div>;
+	// getting the data from our cells in order
+	const cells = useTypedSelector(({ cells: { order, data } }) => {
+		return order.map((id) => {
+			return data[id];
+		});
+	});
+
+	const renderedCells = cells.map((cell) => (
+		<CellListItem key={cell.id} cell={cell} />
+	));
+
+	return <div>{renderedCells}</div>;
 };
 
 export default CellList;
