01e28a0b57ba4efc22f0a0aa40b5a0575f3f6bd2
Gary Hake <Gary.Hake@waystar.com>
2021-05-09 21:34:52 -0400

    converting state to redux

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -3,15 +3,21 @@ import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
 import Resizable from './resizable';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
 
-const CodeCell = () => {
-	const [input, setInput] = useState('');
+interface CodeCellProps {
+	cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 	const [code, setCode] = useState('');
 	const [err, setErr] = useState('');
+	const { updateCell } = useActions();
 
 	useEffect(() => {
 		const timer = setTimeout(async () => {
-			const output = await bundle(input);
+			const output = await bundle(cell.content);
 
 			setCode(output.code);
 			setErr(output.err);
@@ -20,15 +26,15 @@ const CodeCell = () => {
 		return () => {
 			clearTimeout(timer);
 		};
-	}, [input]);
+	}, [cell.content]);
 
 	return (
 		<Resizable direction="vertical">
 			<div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
 				<Resizable direction="horizontal">
 					<CodeEditor
-						initalValue="const a = 1;"
-						onChange={(value) => setInput(value)}
+						initalValue={cell.content}
+						onChange={(value) => updateCell(cell.id, value)}
 					/>
 				</Resizable>
 
